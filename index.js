3aedf38b19b152f793f69cd45b36a5031ba56b2f
juangraydev <jwsabala@gmail.com>
2022-11-09 07:35:02 +0800

    fe

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
-app.use(express.static(path.join(__dirname + "/build")))
+app.use(express.static(path.join(__dirname + "/public")))
 app.use(cors());
 
 
 const auth = require('./router/auth');  
 app.get('*', (req,res) =>{
-    res.sendFile((path.join(__dirname + "/build/index.html")));
+    res.sendFile((path.join(__dirname + "/public/index.html")));
 });
 // app.use("/api/v1/auth", auth);
 
