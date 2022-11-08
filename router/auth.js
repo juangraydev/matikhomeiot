8cefc5862b22af49c9a952f6379d6c8711037f9a
juangraydev <jwsabala@gmail.com>
2022-11-08 20:28:32 +0800

    test

--- router/auth.js
+++ router/auth.js
@@ -1,40 +1,41 @@
 const express = require("express");
 const Router = express.Router();
 const jwt = require('jsonwebtoken');
-const connection = require('../util/connection');
+// const connection = require('../util/connection');
 const bcryptjs = require('bcryptjs');
 const saltRounds = 10;  
 
 Router.post("/signin", (req, res) => {
-    console.log("req:", req.body)
-    const {username,password} = req.body;
-    let sql = `SELECT * FROM users WHERE username="${username}" `
+    // console.log("req:", req.body)
+    // const {username,password} = req.body;
+    // let sql = `SELECT * FROM users WHERE username="${username}" `
     
-    connection.query(sql, function (err, result, fields) {
-        if(err) throw err;
-        if(result[0]){
-            console.log("password: ", password, result[0]['password'])
-            bcryptjs.compare(password, result[0].password, function(err, results) {
-                // result == true
-                if(results){         
-                    console.log("result:", result[0].username);
+    // connection.query(sql, function (err, result, fields) {
+    //     if(err) throw err;
+    //     if(result[0]){
+    //         console.log("password: ", password, result[0]['password'])
+    //         bcryptjs.compare(password, result[0].password, function(err, results) {
+    //             // result == true
+    //             if(results){         
+    //                 console.log("result:", result[0].username);
                     
-                    const token = jwt.sign(result[0].username, "matik");
-                    res.json({
-                        token: token, 
-                        displayName: result[0].display, 
-                        username: result[0].username
-                    });
-                }
-            });
-            console.log("value: ", result);
+    //                 const token = jwt.sign(result[0].username, "matik");
+    //                 res.json({
+    //                     token: token, 
+    //                     displayName: result[0].display, 
+    //                     username: result[0].username
+    //                 });
+    //             }
+    //         });
+    //         console.log("value: ", result);
             
 
-        }else {
-            res.status(403).send();
-        }
+    //     }else {
+    //         res.status(403).send();
+    //     }
+    res.json({Test:"test"})
        
-    });
+    // });
 
 
 })
